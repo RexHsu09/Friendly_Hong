ae04006f5bcf47a9487c75e8c4c690a4c9dc12ca
Rex Hsu <rexhsu09@gapp.nthu.edu.tw>
2022-06-13 18:30:51 +0800

    UI_Improvements

--- app/src/main/java/com/example/layout1/MainActivity.kt
+++ app/src/main/java/com/example/layout1/MainActivity.kt
@@ -42,8 +42,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //Homepage()
-            login()
+            Homepage()
+//            login()
         }
     }
 }
